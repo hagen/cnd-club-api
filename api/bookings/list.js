30219aaf0d12012cb7d95fbab8b68c0bbf37546b
Hagen <hagen.dittmer@gmail.com>
2019-11-03 23:32:55 +0000

    Squashed commit of the following
    commit 68a91066 
    Author: Hagen <hagen@redguava.com.au> 
    Date: Mon Nov 04 2019 10:19:56 GMT+1100 (Australian Eastern Daylight Time) 
    
        Correct API version usage
    
    npm audit fix

--- api/bookings/list.js
+++ api/bookings/list.js
@@ -1,7 +1,7 @@
-const { unpackHttp, returnHttp } = require('../../lib/lambda-proxy');
-const { HTTPError } = require('../../models/HTTPError');
+const {unpackHttp, returnHttp} = require('../../lib/lambda-proxy');
+const {HTTPError} = require('../../models/HTTPError');
 const CNDAPI = require('../../lib/cnd');
-const{ md5 } = require('../../lib/utils');
+const {md5} = require('../../lib/utils');
 const moment = require('moment');
 const validate = require('validate.js');
 const validationRules = {
@@ -44,24 +44,19 @@ const validationRules = {
 };
 
 module.exports = {
-  handle, run
+  handle,
+  run
 };
-
-
 
 async function handle(event, context, callback) {
   try {
-    let body = await run(unpackHttp(event))
-    return returnHttp({ statusCode: 200, body }, callback)
-  } catch(e) {
-		console.error(e)
-    return returnHttp(e, callback)
+    let body = await run(unpackHttp(event));
+    return returnHttp({statusCode: 200, body}, callback);
+  } catch (e) {
+    console.error(e);
+    return returnHttp(e, callback);
   }
 }
-
-
-
-
 
 /**
  * Main function to run.
@@ -69,28 +64,29 @@ async function handle(event, context, callback) {
  * @return {[type]}        [description]
  */
 async function run(params) {
-  const { cndToken } = params.auth;
-  const { vehicle_id, start, end, type } = params.queryStringParameters;
+  const {cndToken} = params.auth;
+  const {vehicle_id, start, end, type} = params.queryStringParameters;
 
   let api = new CNDAPI(cndToken);
-  let { reservations } = await fetchReservations(api, { vehicle_id, start, end, type });  
+  let {reservations} = await fetchReservations(api, {
+    vehicle_id,
+    start,
+    end,
+    type
+  });
   return reservations;
 }
-
-
-
-
 
 /**
  * Returns all bookings for the car, between the start/end dates
  * @param {CNDAPI} api The CND API
  * @param {Object} queryParams Query string params to send (all required)
  */
-async function fetchReservations(api, { vehicle_id, start, end, type }) {  
+async function fetchReservations(api, {vehicle_id, start, end, type}) {
   // Vehicle ID must be a number
   // Start and End must match YYYY-MM-DD
   // Type must be in enum
-  let validationResult = runValidation({ vehicle_id, start, end, type });
+  let validationResult = runValidation({vehicle_id, start, end, type});
   if (validationResult) {
     throw new HTTPError(400, {
       name: 'InvalidParameters',
@@ -98,37 +94,41 @@ async function fetchReservations(api, { vehicle_id, start, end, type }) {
       message: validationResult.join('; ')
     });
   }
-  let urlPath = `/vehicles/${vehicle_id}/calendar_events?vlocal_start_at=${start}&vlocal_end_at=${end}`;
+  let urlPath = `/cars/${vehicle_id}/calendar_events?vlocal_start_at=${start}&vlocal_end_at=${end}`;
 
   // Always filter by type, as type is always required
-  let { calendar_events } = await api.get(urlPath);
+  let {calendar_events} = await api.get(urlPath);
   let reservations = calendar_events
     .filter(item => item.type === type)
     // Finally, create a hash against date/time props of the reservation.
     .map(item => ({
       id: item.reservation ? item.reservation.id : item.schedule.id,
       ...item,
-      start_end_hash: md5(JSON.stringify({ 
-        end_at: item.end_at, start_at: item.start_at
-      }))
+      start_end_hash: md5(
+        JSON.stringify({
+          end_at: item.end_at,
+          start_at: item.start_at
+        })
+      )
     }));
-  
-  return { 
+
+  return {
     urlPath,
     reservations
   };
 }
 module.exports.fetchReservations = fetchReservations;
 
-
-
-
 /**
  * Run validate.js rules, and custom rules. Returns first error/set of errors.
  * @param {Params} param All params to validate
  */
-function runValidation({ vehicle_id, start, end, type, status }) {
-  let validateJsErrors = validate({ vehicle_id, start, end, type, status }, validationRules, { format: 'flat' });
+function runValidation({vehicle_id, start, end, type, status}) {
+  let validateJsErrors = validate(
+    {vehicle_id, start, end, type, status},
+    validationRules,
+    {format: 'flat'}
+  );
   if (validateJsErrors) {
     return validateJsErrors;
   }
